77713f7760d5402ed3537c64e433a3dc21122c95
Finn <anthony.torres0926@gmail.com>
2019-10-06 00:29:00 -0600

    Control Flow

--- Memory Game/js/main.js
+++ Memory Game/js/main.js
@@ -1,6 +1,21 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen' , 'queen' , 'king' , 'king'];
+var cardsInPlay = [];
 
-console.log('User flipped ' + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+console.log('User Flipped' + ' ' + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo)
+console.log('User Flipped' + ' ' + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry try again.');
+	}
+};
+
+
